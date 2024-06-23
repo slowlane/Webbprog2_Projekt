94e9fd6ce7385b4854fa6b4ef880fd890f91a5a6
Andreas W <andreas.wickerfalt@gmail.com>
2024-06-23 15:28:27 +0200

    Joels ändringar

--- script.js
+++ script.js
@@ -4,15 +4,15 @@ const messageInput = document.getElementById("message");
 const button = document.getElementById("submit");
 
 
-if(button){
+if (button) {
     button.addEventListener("click", (event) => {
         event.preventDefault();
 
         let name = nameInput.value;
         let email = emailInput.value;
         let message = messageInput.value;
-    
-        if(validateInput(name, email, message)){
+
+        if (validateInput(name, email, message)) {
             nameInput.value = "";
             emailInput.value = "";
             messageInput.value = "";
@@ -21,13 +21,13 @@ if(button){
 }
 
 
-function validateInput(name, email, message){
+function validateInput(name, email, message) {
 
-    if(!validateEmail(email)){
+    if (!validateEmail(email)) {
         alert("Felaktigt format på email.")
         return;
     }
-    if(!name || name == "" || !message || message == ""){
+    if (!name || name == "" || !message || message == "") {
         alert("Dubbelkolla din info. Du har utelämnat namn eller meddelande.");
         return;
     }
@@ -37,7 +37,22 @@ function validateInput(name, email, message){
 
 }
 
-function validateEmail(email){
+function validateEmail(email) {
     const re = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
     return re.test(email);
 }
+
+let slideIndex = 0;
+showSlides();
+
+function showSlides() {
+    let i;
+    let slides = document.getElementsByClassName("mySlides");
+    for (i = 0; i < slides.length; i++) {
+        slides[i].style.display = "none";
+    }
+    slideIndex++;
+    if (slideIndex > slides.length) { slideIndex = 1 }
+    slides[slideIndex - 1].style.display = "block";
+    setTimeout(showSlides, 5000); 
+} 
